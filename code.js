fa32681e4dcf1c68e23606038fd5ab69bcf2de2a
Vojtaa <65811166+vojta-dev@users.noreply.github.com>
2021-04-22 15:39:10 +0200

    Add Bootstrap styling

--- code.js
+++ code.js
@@ -2,13 +2,13 @@
 
 let haxInterval;
 
-if (location.toString().includes('/app')) document.body.insertAdjacentHTML('afterbegin', '<div id="haxButtons" style="display: flex; justify-content: center;"><button onclick=startHax()>Start</button><button onclick=logHax()>Log</button><button onclick=document.getElementById(\'haxButtons\').style.display=\'none\'>Hide</button></div>');
+if (location.toString().includes('/app')) document.body.insertAdjacentHTML('afterbegin', '<div id="haxButtons" style="display: flex; justify-content: center; gap: 1rem;"><button class="btn btn-primary" onclick=startHax()>Start</button><button class="btn btn-primary" onclick=logHax()>Log</button><button class="btn btn-primary" onclick=document.getElementById(\'haxButtons\').style.display=\'none\'>Hide</button></div><hr>');
 
 const startHax = (delay = 5000) => haxInterval = setInterval(hax, delay);
 const stopHax = () => clearInterval(haxInterval);
 
 const hax = () => {
-	const translationElements = [...document.getElementsByClassName('localWord')];
+  const translationElements = [...document.getElementsByClassName('localWord')];
   const wordDisplayed = document.getElementById('tfw_word').innerText;
   const answerInputBox = document.getElementById('translateFallingWordAnswer');
   const submitButton = document.getElementById('translateFallingWordSubmitBtn');
@@ -27,20 +27,17 @@ const hax = () => {
 }
 
 const logHax = () => {
-	const translationElements = [...document.getElementsByClassName('localWord')];
-	const array = translationElements.map(element => {
-		const word1 = element.getAttribute('word');
-		const word2 = element.getAttribute('translation');
-		return [word1, word2];
-	});
-	console.table(array);
+  const translationElements = [...document.getElementsByClassName('localWord')];
+  const array = translationElements.map(element => {
+    const word1 = element.getAttribute('word');
+    const word2 = element.getAttribute('translation');
+    return [word1, word2];
+  });
+  console.table(array);
 };
 
-// focus the answer box every 0.1s (just QoL lmao)
+// focus the answer box every 0.1s
 setInterval(() => {
-	try {
-		(document.getElementById('translateFallingWordAnswer').focus());
-	} catch (e) {
-		return;
-	}
+  try { document.getElementById('translateFallingWordAnswer').focus(); }
+  catch (e) { return; }
 }, 100);
