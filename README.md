7a07af67fda05df918f3317b47d3ab55e33def1e
Vojta <65811166+vojta-dev@users.noreply.github.com>
2021-05-08 21:35:02 +0200

    Refactoring, rewording, add more tasks

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # Wocabee Bot
 
-I've only tested it in Firefox and Chrome, but it should work in other browsers.
+I've only tested it in Firefox and Chrome, but it should work in other browsers with their corresponding extensions.
 
 ## Installation
 
-Chrome - download [User JavaScript and CSS extension](https://chrome.google.com/webstore/detail/nbhcbdghjpllgmfilhnhkllmkecfmpld), go to [Wocabee](https://wocabee.app/), click on the extension and then on Add new button. Copy and paste the code from code.js file to the JS text area, click on Save, and then close the tab.
+### Chrome
 
-Firefox - download [javascript addon](https://addons.mozilla.org/en-US/firefox/addon/javascript/), go to [Wocabee](https://wocabee.app/), click on the extension, copy and paste the code from code.js file into the window, and then reload the page.
+1. Download [User JavaScript and CSS extension](https://chrome.google.com/webstore/detail/nbhcbdghjpllgmfilhnhkllmkecfmpld).
+2. Go to [Wocabee website](https://wocabee.app/).
+3. Click on the extension and then on Add new button.
+4. Copy and paste the code from [code.js file](code.js) into the JS text area and hit Save.
+
+### Firefox
+
+1. Download [javascript addon](https://addons.mozilla.org/en-US/firefox/addon/javascript/).
+2. Go to [Wocabee website](https://wocabee.app/).
+3. Click on the extension, copy and paste the code from [code.js file](code.js) into the window, and then reload the page.
 
 ## Usage
 
-When you go to [Wocabee App](https://wocabee.app/app/), and select some package to do, there should (hopefully) be some buttons up top.
+When you go to [Wocabee App](https://wocabee.app/app/), and select some package to do, there should be (hopefully) some buttons at the top.
 
-- **Auto Translate** - toggleable button that so far works only on 2 WocaPoints mode, it just submits the correct answer every 5 seconds.
-- **Auto Focus** - toggleable button that automatically focuses the answer box, so instead of needing to click it, you can type right away (not necessary, just some QOL)
-- **Translate Current Word** - button that tries to show / input the correct answer (without submitting it)
-- **Log Table** - logs a simple table with translations to the console
-- **Log JSON** - logs the translations, but in a json format
-- **Hide** - hides all of the buttons (they will reappear on reload)
+- **Auto Grind** - grind WocaPoints in the "Practice" section
+- **Auto Focus** - automatically focus the answer box, so instead of needing to click it, you can type right away
+- **Solve the current task** - show / input the correct answer (without submitting it)
+- **Log Table** - log a simple table with the translations to the console
+- **Log JSON** - log the translations to the console, but in a json format
+- **Hide** - hide all of the buttons (they will reappear on reload)
--- code.js
+++ code.js
@@ -1,163 +1,240 @@
 // Run this file on Wocabee, for more info, go to README.md file
 
-let hInterval;
-let hRunning = false;
-let hAutoFocusingInterval;
-let hAutoFocusing = true;
+let hGrindInterval;
+let hGrindRunning = false;
+let hAutoFocusInterval;
+let hAutoFocusRunning = true;
 
-if (location.toString().includes('/package_local'))
+const currentURL = location.toString();
+if (currentURL.includes('/package_local') || currentURL.includes('/game')) {
   document.body.insertAdjacentHTML(
     'afterbegin',
     /* html */
     `<div id="hButtons" style="display: flex; flex-flow: wrap; justify-content: center; margin-bottom: 1.5rem; gap: 0.5rem;">
-      <button class="btn btn-danger"    id="hStartButton"     onclick="hStartStop()"   >Auto Translate: OFF</button>
+      <button class="btn btn-danger"    id="hGrind"           onclick="hGrind()"       >Auto Grind: OFF</button>
       <button class="btn btn-success"   id="hAutoFocusButton" onclick="hAutoFocus()"   >Auto Focus: ON</button>
       <div style="width: 100%;"></div>
-      <button class="btn btn-primary"   id="hTranslateButton" onclick="hTranslate()"   >Translate Current Word</button>
+      <button class="btn btn-primary"   id="hTranslateButton" onclick="hSolve()"       >Solve the current task</button>
       <button class="btn btn-warning"   id="hLogButton"       onclick="hLog()"         >Log Table</button>
-      <button class="btn btn-warning"   id="hLogJSONButton"   onclick="hLogJSON()"     >Log JSON</button>
+      <button class="btn btn-warning"   id="hLogJSONButton"   onclick="hLog('JSON')"   >Log JSON</button>
       <button class="btn btn-secondary" id="hHideButton"      onclick="hHideButtons()" >Hide</button>
     </div>`
   );
-
-// --------------------
-//  GET STUFF FROM DOM
-// --------------------
-const get = (id, canBeNull = false) => {
-  return document.getElementById(id) ?? (canBeNull ? null : hError(`Could not find an element with ${id} id!`));
-};
-
-const hTranslations = () => [...document.getElementsByClassName('localWord')];
-const hTranslationWords = () => hTranslations().map((element) => [element.getAttribute('word'), element.getAttribute('translation')]);
-const hWordDisplayed = () => get('tfw_word').innerText;
-const hAnswerBox1 = () => get('translateFallingWordAnswer', true);
-const hAnswerBox2 = () => get('translateWordAnswer', true);
-const hAnswerBox3 = () => get('missingWordAnswer', true);
-const hSubmitButton = () => get('translateFallingWordSubmitBtn');
+}
 
 // -----------
 //  UTILITIES
 // -----------
+const hGet = (id, canBeNull = false) => {
+  const element = document.getElementById(id);
+
+  // normal behavior
+  if (element !== null) return element;
+
+  // element is null
+  if (canBeNull) return null;
+  hError(`Could not find an element with ${id} id!`);
+};
+
 const hError = (message) => {
   const errorMessage = `THERE WAS AN ERROR WITH THE WOCABEE BOT\n${message}\nStopping the program...`;
-  clearInterval(hInterval);
+
+  clearInterval(hGrindInterval);
+
   alert(errorMessage);
   console.error(errorMessage);
 };
 
-const hGetTranslation = (word) => {
-  let translations = hTranslationWords();
+const hGetTranslations = () => {
+  const translationElements = [...document.getElementsByClassName('localWord')];
+
+  const translations = translationElements.map((element) => {
+    const word1 = element.getAttribute('word');
+    const word2 = element.getAttribute('translation');
+    return [word1, word2];
+  });
+
+  return translations;
+};
+
+const hGetTranslationFor = (word) => {
+  let translations = hGetTranslations();
   let translation = null;
 
-  translations.map(([word1, word2]) => {
+  translations.forEach(([word1, word2]) => {
     if (word === word1) translation = word2;
     else if (word === word2) translation = word1;
   });
 
   if (translation === null) hError(`Could not find a translation for ${word}!`);
+
   return translation;
 };
 
 // ---------
 //  BUTTONS
 // ---------
-const hStartStop = (delay = 5000) => {
-  const hButton = get('hStartButton');
-  if (hRunning) {
-    clearInterval(hInterval);
-    hButton.innerText = 'Auto Translate: OFF';
-    hButton.classList = 'btn btn-danger';
+const hGrind = (delay = 5000) => {
+  const button = hGet('hGrind');
+
+  const func = () => {
+    const wordDisplayed = hGet('tfw_word').innerText;
+    const answerBox = hGet('translateFallingWordAnswer');
+    const submitButton = hGet('translateFallingWordSubmitBtn');
+    const translation = hGetTranslationFor(wordDisplayed);
+
+    answerBox.value = translation;
+
+    hSubmitButton.disabled = false;
+    hSubmitButton.click();
+  };
+
+  if (hGrindRunning) {
+    clearInterval(hGrindInterval);
+    button.innerText = 'Auto Translate: OFF';
+    button.classList = 'btn btn-danger';
   } else {
-    hInterval = setInterval(() => {
-      hAnswerBox1().value = hGetTranslation(hWordDisplayed());
-      hSubmitButton().disabled = false;
-      hSubmitButton().click();
-    }, delay);
-    hButton.innerText = 'Auto Translate: ON';
-    hButton.classList = 'btn btn-success';
+    hGrindInterval = setInterval(func, delay);
+    button.innerText = 'Auto Translate: ON';
+    button.classList = 'btn btn-success';
   }
-  hRunning = !hRunning;
+
+  hGrindRunning = !hGrindRunning;
 };
 
-const hAutoFocus = () => {
-  const hButton = get('hAutoFocusButton');
-  if (hAutoFocusing) {
-    clearInterval(hAutoFocusingInterval);
-    hButton.innerText = 'Auto Focus: OFF';
-    hButton.classList = 'btn btn-danger';
+const hAutoFocus = (delay = 100) => {
+  const button = hGet('hAutoFocusButton');
+
+  const func = () => {
+    const answerBoxIds = ['translateFallingWordAnswer', 'translateWordAnswer', 'missingWordAnswer'];
+    try {
+      answerBoxIds.forEach((id) => {
+        hGet(id, true).focus();
+      });
+    } catch (e) {
+      return;
+    }
+  };
+
+  if (hAutoFocusRunning) {
+    clearInterval(hAutoFocusInterval);
+    button.innerText = 'Auto Focus: OFF';
+    button.classList = 'btn btn-danger';
   } else {
-    hAutoFocusingInterval = setInterval(() => {
-      try {
-        hAnswerBox1().focus();
-        hAnswerBox2().focus();
-        hAnswerBox3().focus();
-      } catch (e) {
-        return;
-      }
-    }, 100);
-    hButton.innerText = 'Auto Focus: ON';
-    hButton.classList = 'btn btn-success';
+    hAutoFocusInterval = setInterval(func, delay);
+    button.innerText = 'Auto Focus: ON';
+    button.classList = 'btn btn-success';
   }
-  hAutoFocusing = !hAutoFocusing;
+
+  hAutoFocusRunning = !hAutoFocusRunning;
 };
 
-const hTranslate = () => {
-  [
-    ['input', 'translateFallingWord', 'tfw_word', 'translateFallingWordAnswer', 'translateFallingWordSubmitBtn'],
-    ['input', 'translateWord', 'q_word', 'translateWordAnswer', 'translateWordSubmitBtn'],
-    ['choose', 'completeWord', 'completeWordQuestion'],
-    ['choose', 'chooseWord', 'ch_word'],
-    ['multiChoose', 'findPair', 'q_words', 'a_words'],
-    ['transcribe', 'transcribe', 'q_word', 'transcribeAnswerWord', 'transcribeSubmitBtn'], // the word is not stored in the container, but somewhere else... Why? idk
-  ].forEach((hId) => {
-    const type = hId[0];
-    const isContainerVisible = get(hId[1]).style.display !== 'none';
+const hSolve = () => {
+  // prettier-ignore
+  const tasks = [
+    // container id            task type       word id                      input box id                  submit button id
+    [ 'translateFallingWord', 'input',        'tfw_word',                   'translateFallingWordAnswer', 'translateFallingWordSubmitBtn' ],
+    [ 'translateWord',        'input',        'q_word',                     'translateWordAnswer',        'translateWordSubmitBtn'        ],
+    [ 'describePicture',      'input',        'q_word',                     'describePictureAnswer',      'describePictureSubmitBtn'      ],
+    [ 'transcribe',           'transcribe',   'q_word',                     'transcribeAnswerWord',       'transcribeSubmitBtn'           ],
+    [ 'chooseWord',           'choose',      ['ch_word','chooseWordAnswer']                                                               ],
+    [ 'findPair',             'multiChoose', ['q_words','a_words']                                                                        ],
+    [ 'completeWord',         'other',        'completeWordQuestion'                                                                      ],
+    [ 'choosePicture',        'other',        'choosePictureWord'                                                                         ],
+    [ 'addMissingWord',       'other',        'q_sentence'                                                                                ],
+  ];
+
+  tasks.forEach((Ids) => {
+    const [container, taskType, word, inputBox, submitButton] = Ids;
+    const isContainerVisible = hGet(container).style.display !== 'none';
 
     if (isContainerVisible) {
-      const getElements = () => hId.slice(2).map((id) => get(id));
+      switch (taskType) {
+        case 'input': {
+          const wordElement = hGet(word);
+          const translation = hGetTranslationFor(wordElement.innerText);
 
-      if (type === 'input') {
-        const [word, inputBox, submitButton] = getElements();
+          hGet(inputBox).value = translation;
+          hGet(submitButton).disabled = false;
 
-        inputBox.value = hGetTranslation(word.innerText);
-        submitButton.disabled = false;
-      } else if (type === 'choose') {
-        const [word] = getElements();
+          break;
+        }
 
-        word.innerText = hGetTranslation(word.innerText);
-      } else if (type === 'multiChoose') {
-        const [words1, words2] = getElements();
+        case 'transcribe': {
+          const transcription = hGet(word).innerText;
 
-        [...words1.getElementsByClassName('fp_q')].map((word1) => {
-          [...words2.getElementsByClassName('fp_a')].map((word2) => {
-            if (word1.getAttribute('w_id') === word2.getAttribute('w_id')) {
-              word1.classList = 'fp_q btn btn-block btn-lg btn-warning';
-              word2.classList = 'fp_a btn btn-block btn-lg btn-warning';
+          hGet(inputBox).value = transcription;
+          hGet(submitButton).disabled = false;
+
+          break;
+        }
+
+        case 'choose': {
+          const word1 = hGet(word[0]);
+          const words2 = [...document.getElementsByClassName(word[1])];
+          const translation = hGetTranslationFor(word1.innerText);
+
+          words2.forEach((word2) => {
+            if (word2.innerText === translation) {
+              word2.classList = 'chooseWordAnswer btn btn-lg btn-warning btn-block';
             }
           });
-        });
-      } else if (type === 'transcribe') {
-        const [word, inputBox, submitButton] = getElements();
 
-        inputBox.value = word.innerText;
-        submitButton.disabled = false;
+          break;
+        }
+
+        case 'multiChoose': {
+          const words1 = [...hGet(word[0]).getElementsByClassName('fp_q')];
+          const words2 = [...hGet(word[1]).getElementsByClassName('fp_a')];
+
+          words1.forEach((word1) => {
+            words2.forEach((word2) => {
+              if (word1.getAttribute('w_id') === word2.getAttribute('w_id')) {
+                word1.classList = 'fp_q btn btn-block btn-lg btn-warning';
+                word2.classList = 'fp_a btn btn-block btn-lg btn-warning';
+              }
+            });
+          });
+
+          break;
+        }
+
+        case 'other': {
+          const wordElement = hGet(word);
+          const translation = hGetTranslationFor(wordElement.innerText);
+
+          wordElement.innerText = translation;
+
+          break;
+        }
       }
     }
   });
 };
 
-const hLog = () => console.table(hTranslationWords());
+const hLog = (format = 'table') => {
+  const translations = hGetTranslations();
 
-const hLogJSON = () => {
-  console.log(
-    JSON.stringify(
-      hTranslationWords().map((translation) => {
-        return { word1: translation[0], word2: translation[1] };
-      })
-    )
-  );
+  switch (format) {
+    case 'table':
+      console.table(translations);
+      break;
+
+    case 'JSON':
+      let translationsJSON = translations.map((translation) => {
+        return {
+          word1: translation[0],
+          word2: translation[1],
+        };
+      });
+
+      translationsJSON = JSON.stringify(translationsJSON);
+
+      console.log(translationsJSON);
+      break;
+  }
 };
 
 const hHideButtons = () => {
-  get('hButtons').style.display = 'none';
+  hGet('hButtons').style.display = 'none';
 };
